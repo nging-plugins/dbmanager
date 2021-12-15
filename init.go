40433f3f19ea7cf79ed8a196667dad628d2207eb
Hank Shen <swh@admpub.com>
2021-12-15 13:52:17 +0800

    update

--- init.go
+++ init.go
@@ -1,8 +1,11 @@
 package dbmanager
 
 import (
+	"github.com/nging-plugins/dbmanager/pkg/handler"
 	_ "github.com/nging-plugins/dbmanager/pkg/library/setup"
 )
+
+var LeftNavigate = handler.LeftNavigate
 
 func init() {
 
