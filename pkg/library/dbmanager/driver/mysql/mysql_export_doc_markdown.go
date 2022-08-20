0e93b565c1aeed6119d1e9b7dea406ef9ccc4ed3
Hank Shen <swh@admpub.com>
2022-08-20 12:51:50 +0800

    update

--- pkg/library/dbmanager/driver/mysql/mysql_export_doc_markdown.go
+++ pkg/library/dbmanager/driver/mysql/mysql_export_doc_markdown.go
@@ -23,8 +23,8 @@ func (a *mysqlExportMarkdownDoc) Open(c echo.Context) error {
 }
 
 func (a *mysqlExportMarkdownDoc) Write(c echo.Context, table *TableStatus, fields []*Field) error {
-	c.Response().Write([]byte(`## ` + table.Name.String + "\n"))
-	c.Response().Write([]byte(`> ` + table.Comment.String + "\n"))
+	c.Response().Write([]byte(`## ` + table.Name.String + "\n\n"))
+	c.Response().Write([]byte(`> ` + table.Comment.String + "\n\n"))
 	c.Response().Write([]byte(`| ` + c.T(`字段名`) + ` | ` + c.T(`数据类型`) + ` | ` + c.T(`说明`) + ` |` + "\n"))
 	c.Response().Write([]byte(`| :------------ | :------------ | :------------ |` + "\n"))
 	for _, v := range fields {
@@ -43,10 +43,11 @@ func (a *mysqlExportMarkdownDoc) Write(c echo.Context, table *TableStatus, field
 		}
 		c.Response().Write([]byte(`| ` + v.Field + ` | ` + dataType + ` | ` + v.Comment + ` |` + "\n"))
 	}
+	c.Response().Write([]byte("\n"))
 	return nil
 }
 
 func (a *mysqlExportMarkdownDoc) Close(c echo.Context) error {
-	c.Response().Write([]byte("\n"))
+	c.Response().Write([]byte("\n\n"))
 	return nil
 }
