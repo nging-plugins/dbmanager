e83d1d898ad6e4cd5be00bdc03e1f6f6d891b7c7
Hank Shen <swh@admpub.com>
2022-08-20 12:57:44 +0800

    update

--- pkg/library/dbmanager/driver/mysql/mysql_export_doc_html.go
+++ pkg/library/dbmanager/driver/mysql/mysql_export_doc_html.go
@@ -19,7 +19,7 @@ func (a *mysqlExportHTMLDoc) Open(c echo.Context) error {
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEEventStream)
 	encodedName := echo.URLEncode(a.dbName+`_doc.html`, true)
 	c.Response().Header().Set(echo.HeaderContentDisposition, "attachment; filename="+encodedName+"; filename*=utf-8''"+encodedName)
-	c.Response().Write([]byte(`<doctype html><html><head><title>Table Documention</title><link rel="stylesheet" href="` + common.BackendURL(c) + `/public/assets/backend/js/bootstrap/dist/css/bootstrap.min.css?t=20220807115920" /></head><body><div class="row"><div class="col-md-12">`))
+	c.Response().Write([]byte(`<doctype html><html><head><title>Table Documention</title><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="` + common.BackendURL(c) + `/public/assets/backend/js/bootstrap/dist/css/bootstrap.min.css?t=20220807115920" /></head><body><div class="container"><div class="row"><div class="col-md-12">`))
 	return nil
 }
 
@@ -51,6 +51,6 @@ func (a *mysqlExportHTMLDoc) Write(c echo.Context, table *TableStatus, fields []
 }
 
 func (a *mysqlExportHTMLDoc) Close(c echo.Context) error {
-	c.Response().Write([]byte(`</div></div></body></html>`))
+	c.Response().Write([]byte(`</div></div></div></body></html>`))
 	return nil
 }
