e1dbd6e815dc82ff70ae2c5497d3ce217f4e8e87
Hank Shen <swh@admpub.com>
2025-01-08 18:51:18 +0800

    update

--- application/handler/navigate.go
+++ application/handler/navigate.go
@@ -1,82 +1,85 @@
 package handler
 
-import "github.com/coscms/webcore/library/navigate"
+import (
+	"github.com/coscms/webcore/library/navigate"
+	"github.com/webx-top/echo"
+)
 
 var LeftNavigate = &navigate.Item{
 	Display: true,
-	Name:    `数据库`,
+	Name:    echo.T(`数据库`),
 	Action:  `db`,
 	Icon:    `table`,
 	Children: &navigate.List{
 		{
 			Display: true,
-			Name:    `数据库账号`,
+			Name:    echo.T(`数据库账号`),
 			Action:  `account`,
 		},
 		{
 			Display: true,
-			Name:    `添加账号`,
+			Name:    echo.T(`添加账号`),
 			Action:  `account_add`,
 			Icon:    `plus`,
 		},
 		{
 			Display: false,
-			Name:    `修改账号`,
+			Name:    echo.T(`修改账号`),
 			Action:  `account_edit`,
 		},
 		{
 			Display: false,
-			Name:    `删除账号`,
+			Name:    echo.T(`删除账号`),
 			Action:  `account_delete`,
 		},
 		{
 			Display: true,
-			Name:    `连接数据库`,
+			Name:    echo.T(`连接数据库`),
 			Action:  ``,
 		},
 		{
 			Display: true,
-			Name:    `表结构同步`,
+			Name:    echo.T(`表结构同步`),
 			Action:  `schema_sync`,
 		},
 		{
 			Display: false,
-			Name:    `新增同步方案`,
+			Name:    echo.T(`新增同步方案`),
 			Action:  `schema_sync_add`,
 		},
 		{
 			Display: false,
-			Name:    `修改同步方案`,
+			Name:    echo.T(`修改同步方案`),
 			Action:  `schema_sync_edit`,
 		},
 		{
 			Display: false,
-			Name:    `删除同步方案`,
+			Name:    echo.T(`删除同步方案`),
 			Action:  `schema_sync_delete`,
 		},
 		{
 			Display: false,
-			Name:    `预览表结构差异`,
+			Name:    echo.T(`预览表结构差异`),
 			Action:  `schema_sync_preview`,
 		},
 		{
 			Display: false,
-			Name:    `执行表结构同步`,
+			Name:    echo.T(`执行表结构同步`),
 			Action:  `schema_sync_run`,
 		},
 		{
 			Display: false,
-			Name:    `表结构同步日志列表`,
+			Name:    echo.T(`表结构同步日志列表`),
 			Action:  `schema_sync_log/:id`,
 		},
 		{
 			Display: false,
-			Name:    `表结构同步日志详情`,
+			Name:    echo.T(`表结构同步日志详情`),
 			Action:  `schema_sync_log_view/:id`,
 		},
 		{
 			Display: false,
-			Name:    `删除表结构同步日志`,
+			Name:    echo.T(`删除表结构同步日志`),
 			Action:  `schema_sync_log_delete`,
 		},
 	},
